0cedce108a62bfd1deadaa0ba5258a824281e30f
Matex2395 <mateorgafjo@gmail.com>
2025-01-16 10:26:09 -0500

    Registrar el Servicio El servicio de Google Cloud ha sido registrado como Singleton

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using ImageMS.Interfaces;
+using ImageMS.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add Google Cloud Service
+builder.Services.AddSingleton<IGoogleCloudStorageService, GoogleCloudStorageService>();
 
 var app = builder.Build();
 
